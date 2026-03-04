19a540bde33506bbbc42f34808a5a514a26a3a55
Leonardo Alves <bielribeirogamer@gmail.com>
2026-03-04 14:48:29 +0000

    File changes

--- src/pages/ContratoDetalhe.jsx
+++ src/pages/ContratoDetalhe.jsx
@@ -14,8 +14,6 @@ import AditivoForm from "@/components/contratos/AditivoForm.jsx";
 import ItensAgrupados from "@/components/contratos/ItensAgrupados.jsx";
 import EmpenhoForm from "@/components/contratos/EmpenhoForm.jsx";
 
-const fmt = (v) => new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(v || 0);
-
 const NATUREZA_LABELS = {
   "339039_servico": "339039 – Serviços (Manutenção)",
   "339030_material": "339030 – Material de Consumo"
